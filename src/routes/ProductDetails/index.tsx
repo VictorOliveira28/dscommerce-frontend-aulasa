8c192d7cd5578320f4a853b9d5a4a9d2c98c75dd
Victor Oliveira <victor.oliveira20@icloud.com>
2025-02-03 18:19:46 -0300

    Props

--- src/routes/ProductDetails/index.tsx
+++ src/routes/ProductDetails/index.tsx
@@ -1,28 +1,47 @@
-import './styles.css';
+import "./styles.css";
 import ButtonInverse from "../../components/ButtonInverse";
 import ButtonPrimary from "../../components/ButtonPrimary";
 import HeaderClient from "../../components/HeaderClient";
-import ProductCategory from "../../components/ProductCategory";
 import SectionDetails from "../../components/SectionDetails";
+import { ProductDTO } from "../../models/product";
+
+const product: ProductDTO = {
+  id: 2,
+  name: "Smart TV",
+  description: "Esta TV é foda",
+  imgUrl:
+    "https://github.com/devsuperior/dscatalog-resources/blob/master/backend/img/2-big.jpg?raw=true",
+  price: 2500.99,
+  categories: [
+    {
+      id: 2,
+      name: "Eletronicos",
+    },
+    {
+      id: 3,
+      name: "Computadores",
+    },
+    {
+      id: 4,
+      name: "Importados",
+    },
+  ],
+};
 
 export default function ProductDetails() {
-    
-    return (
-        <>
-            <HeaderClient/>    
-            <main>
-              <section id="product-details-section" className="dsc-container">
-                  <SectionDetails />
-                  <div className="dsc-category-container">
-                    <ProductCategory />
-                    <ProductCategory />
-                  </div>
-                <div className="dsc-btn-page-container">
-                  <ButtonPrimary />
-                  <ButtonInverse />
-                </div>
-              </section>
-            </main>
-            </>
-    );
+  return (
+    <>
+      <HeaderClient />
+      <main>
+        <section id="product-details-section" className="dsc-container">
+          <SectionDetails product={product} />
+
+          <div className="dsc-btn-page-container">
+            <ButtonPrimary />
+            <ButtonInverse />
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
