b56d49602131bc9651154cf2b5daf81cc6c51dca
Victor Oliveira <victor.oliveira20@icloud.com>
2025-04-27 12:43:24 -0300

    Catch request error

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -2,7 +2,7 @@ import "./styles.css";
 import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import SectionDetails from "../../../components/SectionDetails";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { ProductDTO } from "../../../models/product";
 import * as productService from "../../../services/product-service";
@@ -12,11 +12,18 @@ export default function ProductDetails() {
 
   const params = useParams();
 
+  const navigate = useNavigate();
+
   useEffect(() => {
-    productService.findById(Number(params.productId)).then((response) => {
-      console.log(response.data);
-      setProduct(response.data);
-    });
+    productService
+      .findById(Number(params.productId))
+      .then((response) => {
+        console.log(response.data);
+        setProduct(response.data);
+      })
+      .catch(() => {
+        navigate("/");
+      });
   });
 
   return (
