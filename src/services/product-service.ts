f4e54129ce31174c0c072f5e8a61f6845820959a
Victor Oliveira <victor.oliveira20@icloud.com>
2025-05-28 15:08:32 -0300

    Searchbar onSearch event

--- src/services/product-service.ts
+++ src/services/product-service.ts
@@ -1,11 +1,27 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import { BASE_URL } from "../utils/system";
 
-export function findAll() {
-    return axios.get(`${BASE_URL}/products?page=0&size=12&sort=name,desc`)
+export function findPageRequest(
+  page: number,
+  name: string,
+  size = 12,
+  sort = "name"
+) {
+  const config: AxiosRequestConfig = {
+    method: "GET",
+    baseURL: BASE_URL,
+    url: "/products",
+    params: {
+      page,
+      name,
+      size,
+      sort,
+    },
+  };
+
+  return axios(config);
 }
 
-export function findById(id: number){
-    return axios.get(`${BASE_URL}/products/${id}`);
+export function findById(id: number) {
+  return axios.get(`${BASE_URL}/products/${id}`);
 }
-
