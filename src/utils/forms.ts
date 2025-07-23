d386ece2ba83f09a466591503ada1e2ef1792849
Victor Oliveira <victor.oliveira20@icloud.com>
2025-07-23 08:05:02 -0300

    Forms update

---
+++ src/utils/forms.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export function update(inputs: any, name: string, newValue: any) {
+  return {
+    ...inputs,
+    [name]: { ...inputs[name], value: newValue },
+  };
+}
