befef64d1149e65b940a87d7d8bc65eae70ad0ff
Victor Oliveira <victor.oliveira20@icloud.com>
2025-07-23 07:55:25 -0300

    Custom FormInput

---
+++ src/components/FormInput/index.tsx
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export default function FormInput(props: any) {
+  const { validation, ...inputProps } = props;
+  return <input {...inputProps} />;
+}
--- src/utils/system.ts
+++ src/utils/system.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = "http://localhost:8081";
 
 export const CART_KEY = "com.victoroliveira.catalogo/Cart";
 export const TOKEN_KEY = "com.victoroliveira.catalogo/Token";
