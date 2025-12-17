757a610639667affd261a7dfa68989524b08c57f
Victor Oliveira <victor.oliveira20@icloud.com>
2025-12-17 08:33:49 -0300

    dirty and validate all

--- src/utils/forms.ts
+++ src/utils/forms.ts
@@ -45,6 +45,43 @@ export function toDirty(inputs: any, name: string) {
   };
 }
 
+export function toDirtyAll(inputs: any) {
+  const newInputs: any = {};
+  for (var name in inputs) {
+    newInputs[name] = { ...inputs[name], dirty: "true" };
+  }
+  return newInputs;
+}
+
+export function validateAll(inputs: any) {
+  const newInputs: any = {};
+
+  for (var name in inputs) {
+    if (inputs[name].validation) {
+      const isInvalid = !inputs[name].validation(inputs[name].value);
+      newInputs[name] = { ...inputs[name], invalid: isInvalid.toString() };
+    } else {
+      newInputs[name] = { ...inputs[name] };
+    }
+  }
+
+  return newInputs;
+}
+
+export function dirtyAndValidateAll(inputs: any) {
+  return validateAll(toDirtyAll(inputs));
+}
+
+export function hasAnyInvalid(inputs: any) {
+  for (var name in inputs) {
+    if (inputs[name].dirty === "true" && inputs[name].invalid === "true") {
+      return true;
+    }
+  }
+
+  return false;
+}
+
 export function updateAndValidate(inputs: any, name: string, newValue: any) {
   const dataUpdated = update(inputs, name, newValue);
   const dataValidated = validate(dataUpdated, name);
