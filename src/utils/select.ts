8004f223b707ab7704184f0bf67a9ffb8e38f588
Victor Oliveira <victor.oliveira20@icloud.com>
2025-12-17 08:07:16 -0300

    react select styling

---
+++ src/utils/select.ts
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export const selectStyles = {
+  control: (provided: any) => ({
+    ...provided,
+    minHeight: "40px",
+    border: "none",
+    boxShadow: "none",
+    "&:hover": {
+      border: "none",
+    },
+  }),
+  placeholder: (provided: any) => ({
+    ...provided,
+    color: "var(--dsc-color-font-placeholder)",
+  }),
+  indicatorSeparator: (provided: any) => ({
+    ...provided,
+    display: "none",
+  }),
+};
